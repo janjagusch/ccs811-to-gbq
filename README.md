d6b334258d9b0225b04aca48279a846a336eafab
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-01-03 16:16:56 +0100

    Initial version (#1)
    * initial commit
    
    * add bigquery
    
    * fix env var name
    
    * fix super().__init__
    
    * make sure eco2 is >= 400
    
    * remove wrong negation
    
    * add readme
    
    * add more instructions to readme
    
    * rephrasing

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # ccs811-to-gbq
-Retrieving data from a CCS811 eCO2 sensor and sending it to Google BigQuery. 
+
+Reads data from a CCS811 eCO2 sensor and sends it to Google BigQuery.
+
+## Getting Started
+
+### Adjust I2C Bus Speed
+
+This sensor is not supported well for the Raspberry Pi since it uses I2C clock stretching which the Pi cannot do without drastically slowing down the I2C speed (to 10 Kbit/s).
+
+In `/boot/config.txt`, find the line containing "dtparam=i2c_arm=on" and add ",i2c_arm_baudrate=10000" where `10000` is the new speed (10 Kbit/s). **Note the comma.**
+
+Afterwards, reboot.
+
+### Requirements
+
+Install the dependencies in `requirements.txt` and activate the virtual environment.
+
+### Google Cloud Authentication
+
+Your application needs to be authenticated against Google Cloud. We recommend [passing credentials via environment variable](https://cloud.google.com/docs/authentication/production#passing_variable).
+
+### BigQuery Table
+
+You will need a BigQuery table with the following schema
+
+```yaml
+- name: sensor_id
+  type: STRING
+  mode: REQUIRED
+- name: requested_at
+  type: TIMESTAMP
+  mode: REQUIRED
+- name: eco2
+  type: INTEGER
+  mode: NULLABLE
+- name: tvoc
+  type: INTEGER
+  mode: NULLABLE
+```
+
+### Environment Variables
+
+Create a `.env` file (`cp .env.example .env`) and fill in the following information:
+
+* `CCS811_SENSOR_ID`: The ID of the sensor (appears in BigQuery).
+* `GBQ_PROJECT_ID`: The ID of your Google Cloud project, where your BigQuery dataset resides.
+* `GBQ_DATASET_ID`: The ID of your BigQuery dataset, where your table resides.
+* `GBQ_TABLE_ID`: The ID of the table, where your measurements should be stored.
+
+## Running the Application
+
+From your virtual environment, execute:
+
+```sh
+python3 main.py
+```
+
+### Running as Crontab
+
+A simple crontab that runs every 5 minutes could look somewhat like this:
+
+```sh
+*/5 * * * * /usr/bin/env bash -c 'cd $HOME/path-to-your-application && source .venv/bin/activate && python3 main.py > .log 2>&1' > /dev/null 2>&1
+```
---
+++ main.py
@@ -0,0 +1,81 @@
+"""
+Reads data from a CCS811 eCO2 sensor and sends it to Google BigQuery.
+"""
+
+import os
+import time
+from datetime import datetime
+
+import adafruit_ccs811
+import board
+import busio
+from dotenv import load_dotenv
+from google.cloud import bigquery
+
+
+class CCS811(adafruit_ccs811.CCS811):
+    """
+    CCS881 class that can take measurements.
+    """
+
+    _READY_WAIT_TIME = 0.1
+
+    def __init__(self, sensor_id):
+        i2c = busio.I2C(board.SCL, board.SDA)
+        super().__init__(i2c)
+        self._sensor_id = sensor_id
+
+    @classmethod
+    def from_env(cls):
+        """
+        Creates an instance of CCS881 from environment variables.
+        """
+        return cls(os.environ["CCS811_SENSOR_ID"])
+
+    @property
+    def _measurement(self):
+        while not self.data_ready:
+            time.sleep(self._READY_WAIT_TIME)
+        eco2, tvoc = self.eco2, self.tvoc
+        while eco2 < 400:
+            time.sleep(self._READY_WAIT_TIME)
+            eco2, tvoc = self.eco2, self.tvoc
+        return {"eco2": eco2, "tvoc": tvoc}
+
+    @property
+    def measurement(self):
+        """
+        Takes a measurement.
+        """
+        measurement_ = {}
+        measurement_["sensor_id"] = self._sensor_id
+        measurement_["requested_at"] = datetime.now()
+        measurement_.update(self._measurement)
+        return measurement_
+
+
+def _gbq_setup():
+    project_id = os.environ["GBQ_PROJECT_ID"]
+    dataset_id = os.environ["GBQ_DATASET_ID"]
+    table_id = os.environ["GBQ_TABLE_ID"]
+    client = bigquery.Client()
+    dataset = bigquery.dataset.DatasetReference.from_string(
+        f"{project_id}.{dataset_id}"
+    )
+    table = client.get_table(dataset.table(table_id))
+    return client, table
+
+
+def _gbq_insert(measurement, client, table):
+    errors = client.insert_rows(table, [measurement])
+    if errors:
+        raise RuntimeError(errors)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    client, table = _gbq_setup()
+    sensor = CCS811.from_env()
+    measurement = sensor.measurement
+    print(measurement)
+    # _gbq_insert(measurement, client, table)
